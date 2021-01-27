4a05f82a5d96454780478895bd13169e4ecffb5a
msub2 <msub2official@gmail.com>
2021-01-27 00:32:11 -0500

    Removed NuGet Newtonsoft, added De-Panther unity webxr packages

--- Assets/Scripts/Input/DetectLogitechVrPen.cs
+++ Assets/Scripts/Input/DetectLogitechVrPen.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if !UNITY_WEBGL
 using UnityEngine;
 using Valve.VR;
 
@@ -89,3 +90,4 @@ namespace TiltBrush {
     }
   }
 } // namespace TiltBrush
+#endif
--- Assets/Scripts/Input/SteamControllerInfo.cs
+++ Assets/Scripts/Input/SteamControllerInfo.cs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#if !UNITY_WEBGL
 using UnityEngine;
 using Valve.VR;
 
@@ -325,3 +325,4 @@ public class SteamControllerInfo : ControllerInfo {
 }
 
 }  // namespace TiltBrush
+#endif
